783acbd5ee6e4ce2f11c8ae2becae73fba2ca9b5
Mitch Bath <mbath@uncc.edu>
2021-10-09 19:02:37 -0400

    Manually added functions to code

--- static/static_sched.cpp
+++ static/static_sched.cpp
@@ -11,10 +11,35 @@
 extern "C" {
 #endif
 
-float f1(float x, int intensity);
-float f2(float x, int intensity);
-float f3(float x, int intensity);
-float f4(float x, int intensity);
+//float f1(float x, int intensity);
+//float f2(float x, int intensity);
+//float f3(float x, int intensity);
+//float f4(float x, int intensity);
+// Old functions. Undefined reference to f1/f2/f3/f4 when make
+
+float f1(float x, int intensity) {
+  int sign = x > 0;
+  
+  for (int i=0; i< intensity; ++i) {
+    x = x*x;
+    x = sqrt(x);
+  }
+  return (sign?1:-1)*x;
+}
+
+float f2(float x, int intensity) {
+  float realx = f1(x, intensity);
+  return realx*realx;
+}
+
+float f3(float x, int intensity) {
+  return sin(f1(x,intensity));
+}
+
+float f4(float x, int intensity) {
+  return exp(cos(f1(x,intensity)));
+}
+
 
 #ifdef __cplusplus
 }
