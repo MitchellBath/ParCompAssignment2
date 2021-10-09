a5c659057d016dc7b6cb379ef530cf1ebb8bd03d
Mitch Bath <mbath@uncc.edu>
2021-10-09 15:18:10 -0400

    Hello_thread.cpp added

--- hello_thread/hello_thread.cpp
+++ hello_thread/hello_thread.cpp
@@ -2,11 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void* print(void* text) {
+
+  cout<<"I am thread " <<(long)p<< " in nbthreads"<<endl;
+  return NULL;
+}
+
+
 int main (int argc, char* argv[]) {
 
   if (argc < 2) {
     std::cerr<<"usage: "<<argv[0]<<" <nbthreads>"<<std::endl;
     return -1;
+  }
+
+  int threadcount = atol(argv[1]);
+
+  pthread_t hellothreads = new pthread_t[threadcount];
+
+  for (int i = 0; i < threadcount; i++) {
+    pthread_create(&hellothreads[i], NULL, print,(void*)i);
+  }
+  for (int i = 0; i < threadcount; i++) {
+    pthread_join(hellothreads[i], NULL));
+  }
+
+
   }
   
   return 0;
