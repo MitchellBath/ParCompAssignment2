207311d6ad6d941d44ec8a41c75ffb8fbbe58cae
Mitch Bath <mbath@uncc.edu>
2021-10-10 02:26:11 -0400

    fgh

--- sequential_lib.c
+++ sequential_lib.c
@@ -15,6 +15,29 @@ float f4(float x, int intensity);
 }
 #endif
 
+// Undefined reference to functions so I manually added them here
+float f1(float x, int intensity) {
+  int sign = x > 0;
+  
+  for (int i=0; i< intensity; ++i) {
+    x = x*x;
+    x = sqrt(x);
+  }
+  return (sign?1:-1)*x;
+}
+
+float f2(float x, int intensity) {
+  float realx = f1(x, intensity);
+  return realx*realx;
+}
+
+float f3(float x, int intensity) {
+  return sin(f1(x,intensity));
+}
+
+float f4(float x, int intensity) {
+  return exp(cos(f1(x,intensity)));
+}
   
 float sequential_integrate (int argc, char* argv[]) {
   
