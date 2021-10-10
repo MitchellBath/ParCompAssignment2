2864fe6fd1712d1ac386cac41bdb45325545dc2a
Mitch Bath <mbath@uncc.edu>
2021-10-10 02:25:04 -0400

    sequential stuff

--- sequential/sequential.cpp
+++ sequential/sequential.cpp
@@ -6,7 +6,29 @@
 extern "C" {
   float sequential_integrate (int argc, char* argv[]);
 }
+  // Undefined reference to functions so I manually added them here
+float f1(float x, int intensity) {
+  int sign = x > 0;
   
+  for (int i=0; i< intensity; ++i) {
+    x = x*x;
+    x = sqrt(x);
+  }
+  return (sign?1:-1)*x;
+}
+
+float f2(float x, int intensity) {
+  float realx = f1(x, intensity);
+  return realx*realx;
+}
+
+float f3(float x, int intensity) {
+  return sin(f1(x,intensity));
+}
+
+float f4(float x, int intensity) {
+  return exp(cos(f1(x,intensity)));
+}
 int main (int argc, char* argv[]) {
 
   if (argc < 6) {
