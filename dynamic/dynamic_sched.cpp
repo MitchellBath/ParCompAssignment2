182c1bfb058ae527e79177965c62f54577606335
Mitch Bath <mbath@uncc.edu>
2021-10-09 22:44:57 -0400

    threads?

--- dynamic/dynamic_sched.cpp
+++ dynamic/dynamic_sched.cpp
@@ -21,6 +21,33 @@ int next_available_beginning, next_available_end;
 #ifdef __cplusplus
 }
 #endif
+
+// Undefined reference to functions so I manually added them here
+float f1(float x, int intensity) {
+  int sign = x > 0;
+  
+  for (int i=0; i< intensity; ++i) {
+    x = x*x;
+    x = sqrt(x);
+  }
+  return (sign?1:-1)*x;
+}
+
+float f2(float x, int intensity) {
+  float realx = f1(x, intensity);
+  return realx*realx;
+}
+
+float f3(float x, int intensity) {
+  return sin(f1(x,intensity));
+}
+
+float f4(float x, int intensity) {
+  return exp(cos(f1(x,intensity)));
+}
+
+
+
 bool task_done = false;
 int current_start = 0;
 float summed_value = 0;
